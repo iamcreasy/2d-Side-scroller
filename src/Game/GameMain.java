28c92046020ac6bde123a98099f15cbd697625b5
Quazi Irfan <quazirfan@gmail.com>
2016-06-28 14:04:24 -0500

    Add Game Package and GameMain

---
+++ src/Game/GameMain.java
@@ -0,0 +1,11 @@
+package Game;
+
+import Engine.GFrame;
+
+public class GameMain {
+    public static void main(String[] args) {
+        GFrame game = new GFrame();
+
+        game.start();
+    }
+}
