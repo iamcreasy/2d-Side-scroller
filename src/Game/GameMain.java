20481e90d86edd679ef1c6cdfeb8994efd75022f
Quazi Irfan <quazirfan@gmail.com>
2016-06-28 14:15:49 -0500

    Add test RedBox

--- src/Game/GameMain.java
+++ src/Game/GameMain.java
@@ -6,6 +6,8 @@ public class GameMain {
     public static void main(String[] args) {
         GFrame game = new GFrame();
 
+        game.add(new RedBox("The RedBox", 100, 100));
+
         game.start();
     }
 }
