5d261be6a01d5ffeb7c3d272f13ea14daba3dc7b
Quazi Irfan <quazirfan@gmail.com>
2016-07-05 00:12:04 -0500

    Fixed BoxMove contructor

--- src/Game/GameMain.java
+++ src/Game/GameMain.java
@@ -2,11 +2,13 @@ package Game;
 
 import Engine.GFrame;
 
+import java.awt.*;
+
 public class GameMain {
     public static void main(String[] args) {
         GFrame game = new GFrame();
 
-        BoxMoveTest b = new BoxMoveTest("Box", 100, 100);
+        BoxMoveTest b = new BoxMoveTest("Box", 100, 100, 50, 50, Color.RED);
         game.add(b);
         game.canvas.addKeyListener(b);
 //        game.add(new WindowedCanvasTest(GFrame.dimension));
