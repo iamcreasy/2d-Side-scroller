1d9939f27220a04a70cc9b219c257b759e4b4e82
Quazi Irfan <quazirfan@gmail.com>
2016-07-05 22:53:02 -0500

    GameResources interface extends Eventlistener marker interface

--- src/Engine/GameResource.java
+++ src/Engine/GameResource.java
@@ -1,7 +1,8 @@
 package Engine;
 
 import java.awt.*;
+import java.util.EventListener;
 
-public interface GameResource {
+public interface GameResource extends EventListener {
     public void update(float tpf, Graphics2D g);
 }
