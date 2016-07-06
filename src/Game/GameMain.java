07eccaeae24dff0ad64bfb2ce6ebcac8f51378ce
Quazi Irfan <quazirfan@gmail.com>
2016-07-06 01:44:40 -0500

    1000 Boxes

--- src/Game/GameMain.java
+++ src/Game/GameMain.java
@@ -8,11 +8,23 @@ public class GameMain {
     public static void main(String[] args) {
         GFrame game = new GFrame();
 
-        BoxMoveTest b = new BoxMoveTest("Box", 100, 100, 50, 50, Color.RED);
-        game.add(b);
-        game.canvas.addKeyListener(b);
-        game.canvas.addMouseMotionListener(b);
+//        Box b = new Box("Box", 100, 100, 100, 100, Color.RED, 100);
+//        game.add(b);
+//        game.canvas.addKeyListener(b);
+//        game.canvas.addMouseMotionListener(b);
 //        game.add(new WindowedCanvasTest(GFrame.dimension));
+
+        Box[] boxes = new Box[500];
+        for(int i = 0 ; i<boxes.length; i++)
+            boxes[i] = new Box();
+
+        game.add(boxes);
+
+//        game.add(new BulletSource(game));
+
+//        game.add(AsteroidField.generateField(10));
+
+//        game.add(new Asteroid());
 
         game.start();
     }
