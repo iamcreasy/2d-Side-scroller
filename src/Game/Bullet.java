2613766a91cff4e310b94788f4d3468cf61dea73
Quazi Irfan <quazirfan@gmail.com>
2016-07-07 01:11:39 -0500

    Add Bullet class

---
+++ src/Game/Bullet.java
@@ -0,0 +1,30 @@
+package Game;
+
+import Engine.GameResource;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class Bullet implements GameResource{
+    Rectangle2D bulletShape = new Rectangle2D.Float();
+    float locX, locY, velocity;
+    Color color;
+
+    public Bullet(float locX, float locY) {
+        this.locX = locX;
+        this.locY = locY;
+        this.color = Color.RED;
+        this.velocity = 500f;
+    }
+
+
+    @Override
+    public void update(float tpf, Graphics2D g) {
+        g.setColor(color);
+
+        bulletShape = new Rectangle2D.Float(locX-3, locY-40, 6, 15);    // 3 and 40 are the fix to move the firing location
+        g.fill(bulletShape);
+
+        locY = locY - velocity * tpf;
+    }
+}
