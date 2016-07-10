a874132b436b35f2b33ca3213e55b8b1afca5c65
Quazi Irfan <quazirfan@gmail.com>
2016-07-10 03:02:40 -0500

    Asteroid and AsteroidField adds GameObject and PhysicsObject

--- src/Game/Asteroid.java
+++ src/Game/Asteroid.java
@@ -1,35 +1,90 @@
 package Game;
 
-import Core.GameResource;
+import Core.GameObject;
+import Physics.PhysicsObject;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.util.Random;
 
-public class Asteroid implements GameResource{
-    private Random rand = new Random(System.nanoTime());
-    private int locX, width, height;
-    private float locY, velocity;
-    private Color color;
+public class Asteroid implements GameObject, PhysicsObject {
+    public String tag = "Asteroid";
+    public Random rand = new Random(System.nanoTime());
+    public float locX, locY, width, height, velocity;
+    public Color color;
+    public boolean alive = true;
     Rectangle2D shape = new Rectangle2D.Float();
 
     public Asteroid() {
-        locX = rand.nextInt(550);
-        locY = rand.nextInt(600) - 600;     // -600 to ensure the spawn loc beyond the canvas height
         width = rand.nextInt(100);
-        height = rand.nextInt(100);
-        velocity = rand.nextInt(200);
+        height = rand.nextInt(100)+15;
+        locX = rand.nextInt(580) + 2;
+        locY = - height;
+        velocity = rand.nextInt(250);
 
         color = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
     }
 
+    public void onDestroy(){
+//        try {
+//            Clip clip = AudioSystem.getClip();
+//            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("./Resources/hit.wav"));
+//            clip.open(ais);
+//            clip.start();
+//        }catch (Exception e){
+//            System.out.println("Error occured during audio playback.");
+//        }
+    }
+
+    // GameObject Interface
     @Override
     public void update(float tpf, Graphics2D g) {
         g.setColor(color);
 
-        locY = locY + velocity * tpf;
-        shape.setRect(locX, locY, width, height);
+        if(alive==true) {
+            locY = locY + velocity * tpf;
+            shape.setRect(locX, locY, width, height);
+        } else {
+            locY = locY + velocity * tpf;
+            height = height - (650 * tpf);
+            shape.setRect(locX, locY, width, height);
+        }
 
         g.fill(shape);
+    }
+
+    @Override
+    public String getTag(){
+        return tag;
+    }
+
+    // PhysicsObject Interface
+    @Override
+    public GameObject getGameObject() {
+        return this;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    @Override
+    public Shape getShape() {
+        return this.shape;
+    }
+
+    @Override
+    public float getLocX() {
+        return locX;
+    }
+
+    @Override
+    public float getLocY() {
+        return locY;
     }
 }
--- src/Game/AsteroidField.java
+++ src/Game/AsteroidField.java
@@ -1,13 +1,64 @@
 package Game;
 
-public class AsteroidField {
-    private static Asteroid[] asteroids;
+import Core.GFrame;
+import Core.GameObject;
+import Physics.PhysicsObject;
+import Physics.PhysicsSpace;
 
-    public static Asteroid[] generateField(int number){
-        asteroids = new Asteroid[number];
-        for(int i = 0; i<number;i++)
-            asteroids[i] = new Asteroid();
+import java.awt.*;
 
-        return  asteroids;
+public class AsteroidField implements GameObject{
+    GFrame game;
+    PhysicsSpace physicsSpace;
+
+    long startSpawnTime = System.currentTimeMillis();
+    public long spawnDelay = 25;        // asteroid spawn delay in milliseconds
+    long nextSpawnTime = startSpawnTime + spawnDelay;
+
+    public int asteroidPresent = 0;     // at the beginning of the game there is no Asteroid, hence initialized to Zero
+    public int asteroidLimit = 250;      // max number of asteroids allowed to spawn
+    public Asteroid asteroid;
+
+    // GFrame and PhysicsSpace reference is needed to dynamically update their object list
+    public AsteroidField(GFrame game, PhysicsSpace physicsSpace) {
+        this.game = game;
+        this.physicsSpace = physicsSpace;
+    }
+
+    @Override
+    public void update(float tpf, Graphics2D g) {
+        // Add Asteroid
+        if(asteroidPresent < asteroidLimit && System.currentTimeMillis() >= nextSpawnTime){
+            asteroid = new Asteroid();
+
+            game.add(asteroid);
+            physicsSpace.add(asteroid);
+            asteroidPresent++;
+
+            startSpawnTime = System.currentTimeMillis();
+            nextSpawnTime = startSpawnTime + spawnDelay;
+        }
+
+        // Cycle through list to Remove Asteroid
+        for(int i = 0; i<game.getGameObjectList().size(); i++)
+            if(game.getGameObjectList().get(i) instanceof Asteroid) {
+                if (((Asteroid) game.getGameObjectList().get(i)).alive == true) {
+
+                    // When the asteroid has passed through without hitting anything
+                    if (((Asteroid) game.getGameObjectList().get(i)).getLocY() > GFrame.dimension.getHeight()) {
+                        physicsSpace.remove((PhysicsObject) game.getGameObjectList().get(i));
+                        game.getGameObjectList().remove(i);
+                        asteroidPresent--;
+                    }
+                } else {
+
+                    // When the asteroid has been hit
+                    if(((Asteroid) game.getGameObjectList().get(i)).getShape().getBounds().getHeight() < 10) {
+                        physicsSpace.remove((PhysicsObject) game.getGameObjectList().get(i));
+                        game.getGameObjectList().remove(i);
+                        asteroidPresent--;
+                    }
+                }
+            }
     }
 }
