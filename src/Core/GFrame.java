5504f69cce6299aac23567f26f77122a7b961388
Quazi Irfan <quazirfan@gmail.com>
2016-07-10 03:03:50 -0500

    Reflect the move to GameObject interface

--- src/Core/GFrame.java
+++ src/Core/GFrame.java
@@ -1,11 +1,11 @@
 package Core;
 
+import Game.Asteroid;
+import Game.Bullet;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelListener;
+import java.awt.event.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -14,7 +14,7 @@ public class GFrame {
     private String tempTitle;
     private long gameStartTime;
     private float tpf;
-    private int userfps = 60;    // Set it to zero for unlimited fps
+    private int userfps = 0;    // Set it to zero for unlimited fps
 
     public static String gameTitle;
     public static Dimension dimension;
@@ -29,21 +29,23 @@ public class GFrame {
     public static Canvas canvas;
     public static BufferStrategy bufferStrategy;
 
-    public GFrame() {
+    public static int mX, mY;
+
+    public void init() {
         gameStartTime = System.nanoTime();
-        gameTitle = "GameTitle";
+        gameTitle = "Asteroid";
         dimension = new Dimension(600, 600);
         jFrame = new JFrame();
         canvas = new Canvas();
 
         if(fullscreen) {
-            // correct dimension
+            // Fix dimension
 
             // Fullscreen jFrame and canvas
 
         } else
         {
-            // correct dimension
+            // Fix dimension
 //            crtJfrmDim = new Dimension((int) dimension.getWidth() + 6, (int) dimension.getHeight() + 35); // Insets[top=32,left=3,bottom=3,right=3]
 //            crtCnvsDim = new Dimension((int) dimension.getWidth() + 6, (int) dimension.getHeight() + 35); // Insets[top=32,left=3,bottom=3,right=3]
 
@@ -77,17 +79,59 @@ public class GFrame {
         canvas.createBufferStrategy(2);
         bufferStrategy = canvas.getBufferStrategy();
 
+        // Add simple mouse motion listener
+        canvas.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if(GFrame.paused == false){
+                    GFrame.mX = e.getX();
+                    GFrame.mY = e.getY();
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                if(GFrame.paused == false){
+                    GFrame.mX = e.getX();
+                    GFrame.mY = e.getY();
+                }
+            }
+        });
+
+        canvas.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_P){
+                    if(GFrame.paused == true) {
+                        GFrame.paused = false;
+
+                        try {
+                            Robot robot = new Robot();
+                            robot.mouseMove((int)GFrame.jFrame.getLocationOnScreen().getX() + GFrame.mX, (int)GFrame.jFrame.getLocationOnScreen().getY() + GFrame.mY);
+                        } catch (AWTException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                    else
+                        GFrame.paused = true;
+                }
+
+                if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                    System.exit(0);
+                }
+            }
+        });
+
         // initial dummy value of tpf
         tpf = (float)(System.nanoTime() -  gameStartTime)/1000000000;
     }
 
     public void start(){
-        // Initialize game loop variabless
+        // Initialize game loop variables
         long oneSecInterval = System.nanoTime();
-        long loopStartTime, nextLoopTime = 0; // Requirs initialization for multiple data path
+        long loopStartTime, nextLoopTime = 0; // Requires initialization for multiple data path
         long loopCount = 0;
         long fps = 0;
-
         if(userfps > 0){
             tpf = (1000000000 / userfps)/(float)1000000000;
         }
@@ -117,9 +161,10 @@ public class GFrame {
                 loopCount = 0;
                 oneSecInterval = System.nanoTime();
             }
-            tempTitle = tempTitle.concat(" FPS " + fps + " ; " + gameResourceList.size());
+            tempTitle = tempTitle.concat(" FPS " + fps + " ; " + gameObjectList.size());
             jFrame.setTitle(tempTitle);
 
+            // Thread.sleep() for capped fps, or calculate tpf
             if(userfps > 0) {
                 if(System.nanoTime() < nextLoopTime) {
                     GFrameUtility.sleep((nextLoopTime - System.nanoTime()) / 1000000); // Convert nano sec to mili sec
@@ -129,48 +174,60 @@ public class GFrame {
             }
         }
 
-        // Cleanup
+        // GFrame exit cleanup
     }
 
-//	ArrayList<GameResource> gameResourceList = new ArrayList<>();
-    CopyOnWriteArrayList<GameResource> gameResourceList = new CopyOnWriteArrayList<>();
+//	ArrayList<GameObject> gameObjectList = new ArrayList<>();
+    CopyOnWriteArrayList<GameObject> gameObjectList = new CopyOnWriteArrayList<>();
 
-    public void add(GameResource gameResource){
-        if(gameResource == null)
+    // Add one GameObject
+    public void add(GameObject gameObject){
+        if(gameObject == null)
             throw new NullPointerException();
         else {
-            // add GameResource Object
-            gameResourceList.add(gameResource);
+            // add GameObject
+            gameObjectList.add(gameObject);
 
-            // add Listeners implementation to Canvas
-            if(gameResource instanceof KeyListener)
-                canvas.addKeyListener((KeyListener)gameResource);
+            // get listeners using GameObject.getEventListeners()
+            if(gameObject.getEventListeners() != null && gameObject.getEventListeners().size() >0 ){
+                for(int i = 0; i< gameObject.getEventListeners().size(); i++) {
+                    // Add KeyListener
+                    if(gameObject.getEventListeners().get(i) instanceof KeyListener )
+                        canvas.addKeyListener((KeyListener)gameObject.getEventListeners().get(i));
 
-            if(gameResource instanceof MouseListener)
-                canvas.addMouseListener((MouseListener)gameResource);
+                    // Add MouseListener
+                    if(gameObject.getEventListeners().get(i) instanceof MouseListener )
+                        canvas.addMouseListener((MouseListener)gameObject.getEventListeners().get(i));
 
-            if(gameResource instanceof MouseMotionListener)
-                canvas.addMouseMotionListener((MouseMotionListener)gameResource);
-
-            if(gameResource instanceof MouseWheelListener)
-                canvas.addMouseWheelListener((MouseWheelListener)gameResource);
+                    // Add MouseMotionListener
+                    if (gameObject.getEventListeners().get(i) instanceof MouseMotionListener)
+                        canvas.addMouseMotionListener((MouseMotionListener) gameObject.getEventListeners().get(i));
+                }
+            }
         }
     }
 
-    public void add(GameResource[] gameResource){
-        if(gameResource == null)
+    // Add an array of GameObject
+    public void add(GameObject[] gameObject){
+        if(gameObject == null)
             throw new NullPointerException();
         else
-            for(int i = 0; i<gameResource.length; i++){
-                add(gameResource[i]);
+            for(int i = 0; i<gameObject.length; i++){
+                add(gameObject[i]);
             }
     }
 
+    // return gameObjectList
+    public CopyOnWriteArrayList getGameObjectList(){
+        return gameObjectList;
+    }
+
+    // Call update methods all GameObjects
     private void update(float tpf, Graphics2D g) {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, (int)dimension.getWidth(), (int)dimension.getHeight());
 
-        for(GameResource gameResource : gameResourceList)
-            gameResource.update(tpf, g);
+        for(GameObject gameObject : gameObjectList)
+            gameObject.update(tpf, g);
     }
 }
